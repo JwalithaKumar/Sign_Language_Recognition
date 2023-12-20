59534da00bcd44a78e4c13667c3c43f8a150e546
JwalithaKumar <154355829+JwalithaKumar@users.noreply.github.com>
2023-12-20 22:01:21 +0530

    Update actionrecognition.py

--- Sign_Language/actionrecognition.py
+++ Sign_Language/actionrecognition.py
@@ -24,3 +24,40 @@ st.write("""
 
 
 temp = st.file_uploader("Please Provide the Snap Shot of your Sign")
+#temp = temp.decode()
+
+buffer = temp
+temp_file = NamedTemporaryFile(delete=False)
+if buffer:
+    temp_file.write(buffer.getvalue())
+    st.write(image.load_img(temp_file.name))
+
+
+if buffer is None:
+    st.write("This model is built for public use by Jayeskumar M")
+
+else:
+
+    img = image.load_img(temp_file.name, target_size=(
+        500, 500), color_mode='grayscale')
+
+    # Preprocessing the image
+    pp_img = image.img_to_array(img)
+    pp_img = pp_img/255
+    pp_img = np.expand_dims(pp_img, axis=0)
+
+    # predict
+    preds = cnn.predict(pp_img)
+    if preds >= 0.5:
+        out = ('I am {:.2%} percent confirmed that this is a Tuberculosis case You may Need to consult a Doctor'.format(
+            preds[0][0]))
+
+    else:
+        out = ('I am {:.2%} percent confirmed that this is a Normal case'.format(
+            1-preds[0][0]))
+
+    st.success(out)
+
+    image = Image.open(temp)
+    st.image(image, use_column_width=True)
+
